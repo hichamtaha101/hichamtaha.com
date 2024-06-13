4fefe23be04d52dd23537c2ac9b822e3fe8eabd9
hichamtaha <hichamtaha101@gmail.com>
2024-06-12 21:49:42 -0700

    update some content

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -2,13 +2,13 @@ export const frontendSkills = [
 	{ name: 'Javascript', percentage: 100, icon: 'devicon-javascript-plain' },
 	{ name: 'Vue', percentage: 100, icon: 'devicon-vuejs-plain' },
 	{ name: 'Tailwind', percentage: 100, icon: 'devicon-tailwindcss-plain' },
+	{ name: 'Typescript', percentage: 90, icon: 'devicon-typescript-plain' },
 	{ name: 'Storybook', percentage: 90, icon: 'devicon-storybook-plain' },
 	{ name: 'Nuxt', percentage: 90, icon: 'devicon-nuxtjs-plain' },
 	{ name: 'React', percentage: 90, icon: 'devicon-react-original' },
 	{ name: 'Next', percentage: 90, icon: 'devicon-nextjs-original-wordmark' },
 	{ name: 'SASS', percentage: 75, icon: 'devicon-sass-original' },
 	{ name: 'Wordpress', percentage: 75, icon: 'devicon-wordpress-plain' },
-	{ name: 'Webpack', percentage: 75, icon: 'devicon-webpack-plain' },
 ];
 
 export const backendSkills = [
